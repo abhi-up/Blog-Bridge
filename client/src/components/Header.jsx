2dada29231898ad59fceec2d126094aae1d3b7f1
Abhi <meabhishek1512@gmail.com>
2024-03-23 23:15:51 +0530

    general styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,38 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import Logo from "../images/logo.png"
+import { FaBars } from "react-icons/fa"
+import { AiOutlineClose } from "react-icons/ai"
 
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <nav>
+            <div className="container nav__container">
+                <Link to="/" className="nav__logo">
+                    <img src={Logo} alt="Logo" />
+                </Link>
+
+                <ul className="nav__menu">
+                    <li>
+                        <Link to="/profile">Abhishek Upadhyay</Link>
+                    </li>
+                    <li>
+                        <Link to="/create">Create Post</Link>
+                    </li>
+                    <li>
+                        <Link to="/authors">Authors</Link>
+                    </li>
+                    <li>
+                        <Link to="/logout">Logout</Link>
+                    </li>
+                </ul>
+
+                <button className="nav__toggle-btn">
+                    <AiOutlineClose />
+                </button>
+            </div>
+        </nav>
+    )
 }
 
 export default Header
