e52b3d47c82205e6e0130ff3584283d0b8e32154
Abhi <meabhishek1512@gmail.com>
2024-03-24 12:21:18 +0530

    post details/single post page component created and styled

---
+++ client/src/data.js
@@ -0,0 +1,66 @@
+import blog6 from "./images/blog6.jpg"
+import blog17 from "./images/blog17.jpg"
+import blog24 from "./images/blog24.jpg"
+import blog39 from "./images/blog39.jpg"
+import blog10 from "./images/blog10.jpg"
+import blog1 from "./images/blog1.jpg"
+import blog8 from "./images/blog8.jpg"
+
+export const DUMMY_POSTS = [
+    {
+        id: 1,
+        thumbnail: blog6,
+        category: "Agriculture",
+        title: "The Future of Sustainable Agriculture: Innovations and Practices",
+        desc: "Introduction to the importance of sustainable agriculture; Overview of innovative farming practices (e.g., precision agriculture, vertical farming); Benefits of sustainable agriculture for the environment and food security; Case studies of successful sustainable agriculture initiatives",
+        authorID: 3,
+    },
+    {
+        id: 2,
+        thumbnail: blog17,
+        category: "Business",
+        title: "Navigating the Post-Pandemic Business Landscape: Strategies for Success",
+        desc: "Introduction to the challenges faced by businesses post-pandemic; Importance of digital transformation and online presence; Strategies for adapting to changing consumer behavior; Case studies of businesses that successfully pivoted during the pandemic",
+        authorID: 1,
+    },
+    {
+        id: 3,
+        thumbnail: blog39,
+        category: "Education",
+        title: "The Role of Technology in Modern Education: Enhancing Learning Experiences",
+        desc: "Introduction to the integration of technology in education; Benefits of using educational technology tools (e.g., online learning platforms, VR/AR); Challenges and considerations for implementing technology in education; Case studies of schools or institutions leveraging technology effectively",
+        authorID: 2,
+    },
+    {
+        id: 4,
+        thumbnail: blog10,
+        category: "Entertainment",
+        title: "The Evolution of Streaming Services: A New Era of Entertainment",
+        desc: "Introduction to the rise of streaming services and their impact on traditional media; Overview of popular streaming platforms and their unique offerings; Trends shaping the future of entertainment consumption (e.g., interactive content, personalized recommendations); Case studies of successful streaming services and their business models",
+        authorID: 1,
+    },
+    {
+        id: 5,
+        thumbnail: blog1,
+        category: "Art",
+        title: "Art in the Digital Age: Exploring New Mediums and Platforms",
+        desc: "Introduction to the intersection of art and technology; Overview of digital art mediums (e.g., digital painting, 3D modeling, NFTs); Impact of digital platforms and social media on artists and their work; Case studies of artists embracing digital mediums and platforms",
+        authorID: 1,
+    },
+    {
+        id: 6,
+        thumbnail: blog24,
+        category: "Investment",
+        title: "Diversifying Your Investment Portfolio: Strategies for Long-Term Growth",
+        desc: "Introduction to the importance of diversification in investment; Overview of different investment options (e.g., stocks, bonds, real estate, cryptocurrencies); Strategies for building a diversified investment portfolio; Case studies of successful investors and their diversified portfolios",
+        authorID: 3,
+    },
+    {
+        id: 7,
+        thumbnail: blog8,
+        category: "Weather",
+        title: "Understanding Climate Change: How Weather Patterns Are Shifting Globally",
+        desc: "Introduction to climate change and its impact on global weather patterns; Overview of the science behind climate change and weather extremes; Effects of changing weather patterns on agriculture, ecosystems, and human populations; Case studies of regions experiencing significant weather changes and adaptation strategies",
+        authorID: 4,
+    },
+]
--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,7 +1,104 @@
 import React from "react"
+import PostAuthor from "../components/PostAuthor"
+import { Link } from "react-router-dom"
+import Thumbnail from "../images/blog1.jpg"
 
 const PostDetail = () => {
-    return <div>PostDetail</div>
+    return (
+        <section className="post-detail">
+            <div className="container post-detail__container">
+                <div className="post-detail__header">
+                    <PostAuthor />
+                    <div className="post-detail__buttons">
+                        <Link
+                            to={`/posts/erwew/edit`}
+                            className="btn sm primary"
+                        >
+                            Edit
+                        </Link>
+                        <Link
+                            to={`/posts/erwew/delete`}
+                            className="btn sm danger"
+                        >
+                            Delete
+                        </Link>
+                    </div>
+                </div>
+
+                <h1>This is the post title!</h1>
+                <div className="post-detail__thumbnail">
+                    <img src={Thumbnail} alt="" />
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+                    debitis pariatur perferendis alias eos. A perferendis
+                    provident tempore nemo corporis fuga, inventore nam,
+                    voluptas magni omnis ex hic impedit quibusdam. Id nihil rem,
+                    dignissimos est adipisci quas? Illum, quas aut.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Laboriosam enim eligendi error odio eos dignissimos
+                    cupiditate, nesciunt ut aperiam consectetur incidunt impedit
+                    eius nam cum maxime assumenda explicabo quaerat commodi,
+                    tempore, minus quibusdam distinctio quam ipsa! Natus totam
+                    eligendi molestiae quia rerum, provident non laudantium quos
+                    quaerat ex, porro eum aperiam vitae, itaque amet temporibus?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Distinctio natus sint veritatis, a voluptatibus laudantium
+                    expedita nam similique voluptas, velit dicta odio, quasi
+                    dolores aut odit? Quo, neque? Iusto, pariatur magni dolor
+                    vel voluptas qui, ut tenetur eos saepe, dolores a recusandae
+                    quod delectus itaque laborum suscipit cum temporibus
+                    necessitatibus rem deleniti vitae. Incidunt id provident non
+                    laboriosam! Ipsam iure doloremque magni, recusandae aperiam
+                    excepturi hic porro debitis dolores nobis molestiae
+                    voluptatibus, quae velit magnam beatae laborum ipsa quisquam
+                    architecto libero! Quisquam inventore autem saepe, eligendi
+                    iusto magnam rem error aperiam molestias eos dignissimos
+                    blanditiis praesentium molestiae magni temporibus nam
+                    tempore quas perferendis odio excepturi quo odit!
+                    Exercitationem dolorem molestiae provident.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Expedita ratione minima dolor autem quaerat nihil obcaecati,
+                    qui repudiandae! Odit, minima!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quod qui doloribus consectetur itaque at! Necessitatibus, ut
+                    quas consectetur nesciunt ipsam iste exercitationem ratione
+                    reprehenderit, enim voluptas illo at maiores libero.
+                    Quisquam a dolores, quaerat minus odio, qui corrupti
+                    deserunt aperiam provident, vitae dolore corporis expedita.
+                    Fuga fugit fugiat corrupti, sit quaerat porro iusto
+                    architecto nobis. Cumque aliquam deserunt necessitatibus
+                    consequatur repudiandae temporibus voluptatibus ipsa
+                    nostrum. Incidunt est laboriosam aperiam debitis optio
+                    mollitia, saepe reiciendis libero odit omnis deserunt.
+                    Nostrum voluptates molestias iure amet quas molestiae eum
+                    animi perspiciatis perferendis, provident saepe inventore
+                    repudiandae. Atque ex magnam, voluptates tempore nostrum
+                    consequuntur et facere quod, ad, veniam odit quidem soluta.
+                    Adipisci odio quas nostrum. Et error dolorum eos quis eius,
+                    blanditiis deleniti quas rerum nobis delectus dolore non
+                    provident consectetur hic aliquam, tempore cum architecto?
+                    Cumque dolores ut impedit nihil, nobis alias sed dolor porro
+                    dolorum unde, nemo laudantium? Exercitationem obcaecati quam
+                    doloremque quas numquam, possimus labore deleniti voluptate
+                    libero laudantium dolorum tenetur error enim eos deserunt
+                    ratione. Fugiat dolor id impedit reiciendis nisi, veniam
+                    temporibus, laborum aspernatur fugit molestiae similique
+                    blanditiis at quia facilis enim, eaque magnam commodi eos
+                    asperiores suscipit fuga aut doloribus quis error. Aut
+                    voluptatem quis officia laudantium!
+                </p>
+            </div>
+        </section>
+    )
 }
 
 export default PostDetail
