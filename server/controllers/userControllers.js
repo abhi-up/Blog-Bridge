8e96ecdfdd46d6d3ad6ec629e505f55484291da4
Abhi <meabhishek1512@gmail.com>
2024-03-25 18:42:59 +0530

    get user (user profile) controller

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -87,7 +87,18 @@ const loginUser = async (req, res, next) => {
 // POST: api/users/:id
 // PROTECTED
 const getUser = async (req, res, next) => {
-    res.json("get user")
+    try {
+        const { id } = req.params
+        const user = await User.findById(id).select("-password")
+
+        if (!user) {
+            return next(new HttpError("User not found.", 404))
+        }
+
+        res.status(200).json(user)
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 // ==================== CHANGE USER AVATAR ========================
