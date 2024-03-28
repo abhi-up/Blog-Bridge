3736c9c015028c00750e25fd4fbe68acdc4c6bd4
Abhi <meabhishek1512@gmail.com>
2024-03-28 10:14:52 +0530

    updated: delete post component

--- client/src/pages/DeletePost.jsx
+++ client/src/pages/DeletePost.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate, useLocation } from "react-router-dom"
 import { UserContext } from "../context/userContext"
+import axios from "axios"
 
-const DeletePost = () => {
+const DeletePost = ({ postId: id }) => {
     const navigate = useNavigate()
 
     const { currentUser } = useContext(UserContext)
@@ -15,7 +16,33 @@ const DeletePost = () => {
         }
     }, [])
 
-    return <Link className="btn sm danger">Delete</Link>
+    const removePost = async () => {
+        try {
+            const response = await axios.delete(
+                `http://localhost:5000/api/posts/${id}`,
+                {
+                    withCredentials: true,
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            )
+
+            if (response.status == 200) {
+                if (location.pathname == `/myposts/${currentUser.id}`) {
+                    navigate(0)
+                } else {
+                    navigate("/")
+                }
+            }
+        } catch (error) {
+            console.log("Couldn't delete post.")
+        }
+    }
+
+    return (
+        <Link className="btn sm danger" onClick={() => removePost(id)}>
+            Delete
+        </Link>
+    )
 }
 
 export default DeletePost
